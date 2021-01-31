e4b52365efbbd83a3f8861f622e87138e8fa1237
Will Carter <will.carter@fergusllc.com>
2021-01-31 13:28:33 -0700

    refactor

--- verticalOrderTraversalBinaryTree/votbt.js
+++ verticalOrderTraversalBinaryTree/votbt.js
@@ -1,69 +1,69 @@
-const verticalTraversal = (root) => {
+const getCoordinatesFor = (row) => {
   
-  const getCoordsForTreeWithDepth = (depth) => {
-  
-    const getCoordinatesFor = (row) => {
-  
-      if(row === 0 ) {
-        return [[0,0]]
-      }
-    
-      let index = 0
-      if(row % 2 > 0) {
-        index = 1
-      }
-    
-      let buildArray = []
-      while(index <= row) {
-        buildArray.push(index)
-        if(index < row && index > 0) {
-          buildArray.push(index)
-        }
-        index = index + 2
-      }
-    
-      let prefix = []
-      for(let i = buildArray.length - 1; i >= 0; i--) {
-        let valueToPush = buildArray[i]
-        if(buildArray[i] > 0) {
-          valueToPush = buildArray[i] * -1
-        }
-        prefix.push(valueToPush)
-      }
-    
-      let combined = [...prefix, ...buildArray]
-    
-      let final = combined.map((item) => {
-        return [row, item]
-      })
-    
-      return final
-    
-    }
-  
-    returnArray = []
-    for (let i = 0; i <= depth; i++) {
-      returnArray = [...returnArray, ...getCoordinatesFor(i)]
-    }
-    return returnArray
-  
+  if(row === 0 ) {
+    return [[0,0]]
   }
 
+  let index = 0
+  if(row % 2 > 0) {
+    index = 1
+  }
+
+  let buildArray = []
+  while(index <= row) {
+    buildArray.push(index)
+    if(index < row && index > 0) {
+      buildArray.push(index)
+    }
+    index = index + 2
+  }
+
+  let prefix = []
+  for(let i = buildArray.length - 1; i >= 0; i--) {
+    let valueToPush = buildArray[i]
+    if(buildArray[i] > 0) {
+      valueToPush = buildArray[i] * -1
+    }
+    prefix.push(valueToPush)
+  }
+
+  let combined = [...prefix, ...buildArray]
+
+  let final = combined.map((item) => {
+    return [row, item]
+  })
+
+  return final
+
+}
+
+const getCoordsForTreeWithDepth = (depth) => {
+  
+    
+  returnArray = []
+  for (let i = 0; i <= depth; i++) {
+    returnArray = [...returnArray, ...getCoordinatesFor(i)]
+  }
+  return returnArray
+
+}
+
+const verticalTraversal = (root) => {
+  
   let treeNodeLength = 0
   let rows = 0
   let slots = []
+  
   while(treeNodeLength < root.length) {
     let coords = getCoordsForTreeWithDepth(rows)
     treeNodeLength =+ coords.length
     slots = coords
     rows++
   }
-  
-  let finalArray = []
-  for (let i = 0; i < root.length; i++) {
-    let item = [root[i], slots[i]]
-    finalArray.push(item)
-  }
+    
+  let finalArray = root.map((item, i) => {
+    return  [root[i], slots[i]]
+  })
 
   let maxColumn = 0
   let minColumn = 0
@@ -98,9 +98,12 @@ const verticalTraversal = (root) => {
       finalFinal.push(filtered)
     }
   }
+  
+  //return [[9],[3,15],[20],[7]]
+  console.log(finalFinal)
+    
   return finalFinal
 }
 
-//let root = [1,2,3,4,5,6,7]
-root = [3,9,20,null,null,15,7]
-console.log(verticalTraversal(root))
+
+console.log(verticalTraversal([1,2,3,4,5,6,7]))
