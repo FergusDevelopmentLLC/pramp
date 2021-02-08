2d7fd4da7da511d0dce8df7fbe8a6670b272c2dd
Will Carter <will.carter@fergusllc.com>
2021-02-07 19:06:00 -0700

    add selectionSort.js

---
+++ selectionSort/selectionSort.js
@@ -0,0 +1,20 @@
+let selectionSort = (arr) => {
+  let len = arr.length;
+  for (let i = 0; i < len; i++) {
+      let min = i;
+      for (let j = i + 1; j < len; j++) {
+          if (arr[min] > arr[j]) {
+              min = j;
+          }
+      }
+      if (min !== i) {
+          let tmp = arr[i];
+          arr[i] = arr[min];
+          arr[min] = tmp;
+      }
+  }
+  return arr;
+}
+
+let array = [7, 0, 3, 2, 1, 9, 2]
+console.log(selectionSort(array))
