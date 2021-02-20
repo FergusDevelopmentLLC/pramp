d8b876e2948a736fa01df6e666e868f3daa922a0
Will Carter <will.carter@fergusllc.com>
2021-02-19 23:21:13 -0700

    refactor

--- bubbleSort/bubbleSort.js
+++ bubbleSort/bubbleSort.js
@@ -1,15 +1,22 @@
-let bubbleSort = (inputArr) => {
-  for (let i = 0; i < inputArr.length; i++) {
-      for (let j = 0; j < inputArr.length; j++) {
-          if (inputArr[j] > inputArr[j + 1]) {
-              let tmp = inputArr[j];
-              inputArr[j] = inputArr[j + 1];
-              inputArr[j + 1] = tmp;
-          }
-      }
-  }
-  return inputArr;
-};
+//Bubble sort is based on the idea of repeatedly comparing pairs of adjacent elements and then swapping their positions if they exist in the wrong order.
+//It is one of the more intuitive sorting methods as its algorithm mirrors how our brain generally thinks about sorting - by comparing.
 
-let array = [0, 3, 2, 1, 7, 2]
-console.log(bubbleSort(array))
+const bubbleSort = (sortMe) => {
+  sortMe.forEach((item, pass) => {
+    console.log('pass:', pass)
+    sortMe.forEach((item, j) => {
+      console.log('sortMe', sortMe)
+      if(item > sortMe[j + 1]) {
+        const temp = sortMe[j]
+        sortMe[j] = sortMe[j + 1]
+        sortMe[j + 1] = temp
+      }
+    })
+    console.log('------')
+  })
+  return sortMe
+}
+console.log(bubbleSort([7, 3, 4, 2, 1]))
+
+// https://www.hackerearth.com/practice/algorithms/sorting/bubble-sort/visualize/
+// https://visualgo.net/en/sorting
